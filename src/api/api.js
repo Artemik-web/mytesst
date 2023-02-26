b4d537f4ddee6c1db5080921deae53cb25f4835a
Artemik-web <artemik@163.com>
2023-02-26 11:49:55 +0800

    接口调试

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@
 // const url = 'http://127.0.0.1:3007'
 import axios from 'axios'
 const insatnce = axios.create({
-    baseURL: 'http://127.0.0.1:3007',
+    baseURL: 'http://47.115.214.138:3007',
     timeout: 5000,
     headers: {
 		// 设置后端需要的传参类型
